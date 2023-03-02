2876c89f28cbfd00b39f934739145ad33c2db35c
alireza talebizadeh <alireza.talebizadeh78@gmail.com>
2023-03-02 20:31:07 +0330

    a mini project that created with swiper library

---
+++ js/app.js
@@ -0,0 +1,14 @@
+let swiper = new Swiper(".mySwiper", {
+  slidesPerView: 'auto',
+  spaceBetween: 40,
+  centeredSlides: true,
+  loop: true,
+  grabCursor: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
+
